96ab1a59f665724c589e424d24a8fc786c088831
Bernardo <bemedeiros66@gmail.com>
2023-05-21 01:34:49 -0300

    alteracoes

--- a.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger-menu');
-const navBar = document.querySelector('.menu');
-
-hamburger.addEventListener('click', function() {
-  navBar.classList.toggle('active');
-});
